46907dfbacbcbc8e24e8b53c799c847424b76091
Adrián González Barbosa <adrian.gonzalez.barbosa@gmail.com>
2018-06-29 01:24:30 +0200

    Removed customizations

--- install.sh
+++ install.sh
@@ -4,8 +4,22 @@ git clone https://github.com/agonbar/PDP
 # Go into the repository
 cd PDP/project
 
+#Gain admin rights
+su
+
 # Copy contents from the PHP
-cp lamorisse/* /var/www/html
+cp -r lamorisse/* /var/www/html
+rm /var/ww/html/index.html
 
 # Load de Database
-mysql -u iu -pui < db.sql
+mysql -u root -piu < db.sql
+
+# Install server dependency
+apt-get install php-intl -Y
+a2enmod rewrite
+service apache2 restart
+
+#Install cakephp dependency
+cd /var/ww/html
+wget -O - https://getcomposer.org/installer | php
+php composer.phar install
--- project/lamorisse/src/Controller/CompanysController.php
+++ project/lamorisse/src/Controller/CompanysController.php
@@ -1,72 +1,114 @@
-//src/Controller/CompanysController
-namespace App\Controller
-class CompanysController extends AppController{
-    public $components = ['Flash'];
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Companys Controller
+ *
+ * @property \App\Model\Table\CompanysTable $Companys
+ *
+ * @method \App\Model\Entity\Company[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class CompanysController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
     public function index()
     {
-         $this->set('companys', $this->Companys->find('all'));
+        $this->paginate = [
+            'contain' => ['Users']
+        ];
+        $companys = $this->paginate($this->Companys);
+
+        $this->set(compact('companys'));
     }
 
+    /**
+     * View method
+     *
+     * @param string|null $id Company id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
     public function view($id = null)
     {
-        $company = $this->Companys->get($id);
-        $this->set(compact('company'));
-    }
-    public function add()
-    {
-        $company = $this -> Companys->newEntity();
-        if ($this->request->is('post')) {
-            $company = $this->Companys->patchEntity($company, $this->request->getData());
-            $company->user_id = $this->Auth->user('id');
-            if ($this->Companys->save($company)) {
-                $this->Flash->success(__('Your company has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('Unable to add your company.'));
-        }
+        $company = $this->Companys->get($id, [
+            'contain' => ['Users', 'Requisites']
+        ]);
+
         $this->set('company', $company);
     }
 
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $company = $this->Companys->newEntity();
+        if ($this->request->is('post')) {
+            $company = $this->Companys->patchEntity($company, $this->request->getData());
+            if ($this->Companys->save($company)) {
+                $this->Flash->success(__('The company has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The company could not be saved. Please, try again.'));
+        }
+        $users = $this->Companys->Users->find('list', ['limit' => 200]);
+        $requisites = $this->Companys->Requisites->find('list', ['limit' => 200]);
+        $this->set(compact('company', 'users', 'requisites'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Company id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
     public function edit($id = null)
     {
-    $company = $this->Companys->get($id);
-    if ($this->request->is(['post', 'put'])) {
-        $this->Companys->patchEntity($company, $this->request->getData());
-        if ($this->Companys->save($company)) {
-            $this->Flash->success(__('The company data has been modified.'));
-            return $this->redirect(['action' => 'index']);
+        $company = $this->Companys->get($id, [
+            'contain' => ['Requisites']
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $company = $this->Companys->patchEntity($company, $this->request->getData());
+            if ($this->Companys->save($company)) {
+                $this->Flash->success(__('The company has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The company could not be saved. Please, try again.'));
         }
-        $this->Flash->error(__('The company could not be modified.'));
+        $users = $this->Companys->Users->find('list', ['limit' => 200]);
+        $requisites = $this->Companys->Requisites->find('list', ['limit' => 200]);
+        $this->set(compact('company', 'users', 'requisites'));
     }
 
-    $this->set('company', $company);
-    }
-
-    public function delete($id)
+    /**
+     * Delete method
+     *
+     * @param string|null $id Company id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
     {
         $this->request->allowMethod(['post', 'delete']);
-
         $company = $this->Companys->get($id);
         if ($this->Companys->delete($company)) {
-            $this->Flash->success(__('Company with id: {0} has been deleted.', h($id)));
-            return $this->redirect(['action' => 'index']);
-        }
-    }
-
-    public function isAuthorized($user)
-    {
-        //all registered users can create a company
-        if($this->request->getParam('action') === 'add'){
-            return true;
+            $this->Flash->success(__('The company has been deleted.'));
+        } else {
+            $this->Flash->error(__('The company could not be deleted. Please, try again.'));
         }
 
-        if(in_array($this->request->getParam('action'), ['edit', 'delete'])){
-            $companyId = (int)$this->request->getParam('pass.0');
-            if ($this->Companys->isOwnedBy($companyId, $user['id'])) {
-                return true;
-            }
-        }
-
-        return parent::isAuthorized($user);
+        return $this->redirect(['action' => 'index']);
     }
 }
