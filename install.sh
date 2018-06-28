8c9aee90ce5678fcabe2072d11f9f2054186fcb7
Adrián González Barbosa <adrian.gonzalez.barbosa@gmail.com>
2018-06-29 00:37:50 +0200

    First mod to installer

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 # Clone this repository
-$ git clone https://github.com/agonbar/PDP
+git clone https://github.com/agonbar/PDP
 
 # Go into the repository
-$ cd PDP/proyect
+cd PDP/proyect
 
 # Copy contents from the PHP
-$ cp lamorisse/* /var/www/html
+cp lamorisse/* /var/www/html
 
 # Load de Database
-$ mysql -u iu -pui < db.sql
+mysql -u iu -pui < db.sql
